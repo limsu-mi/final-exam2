dda7036ed364b6a95374ae7babc926a3d32a0671
limsu-mi <limsu-mi>
2017-12-19 11:36:26 +0900

    04

---
+++ main.c
@@ -0,0 +1,27 @@
+#include"func.h"
+
+int main()
+{
+    char *buffer;
+    int teach;
+    char *in;
+    char *out;
+    char name[15];
+    buffer = s_malloc(len);
+    srand((unsigned) time (NULL));
+
+    printf("what is your name? \n");
+    scanf("%s",name);
+    getchar();
+
+    inputs(*buffer, name);
+    in = s_malloc(buffer)+1;
+
+
+
+
+
+
+
+    strcpy(in, buffer)
+
