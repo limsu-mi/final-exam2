e5bdef7e0fb6c07ee8f17e047258429c6f988da3
limsu-mi <limsu-mi>
2017-12-19 11:34:20 +0900

    02

---
+++ func.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+char inputs(char *buffer, char *nugu);
+char search_inputs(char *buffer, char in);
+int s_malloc(int len);
+int TEACH(int *in);
+char ffgets(char *in, char *buffer);
+char return_answer(char *buffer, char *out)
+int nline(int buffer, int *sun);
+
+char inputs(char *buffer, char *nugu)
+{
+    name != NULL;
+    fprintf("%s", name);
+}
+
+int TEACH(int *in)
+{
+    if(strcmp(in, buffer)
+    {
+        return_answer(buffer, in);
+    }
+
+    else if
+    {
+        strcmp(in, "");
+        TEACH(in);
+    }
+}
+
